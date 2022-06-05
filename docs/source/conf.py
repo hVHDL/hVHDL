758a6b0956cc3be968a07095426e9c8635573879
Jari Honkanen <50397096+johonkanen@users.noreply.github.com>
2022-06-05 10:45:00 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'hlsVHDL'
+copyright = '2022, Jari Honkanen'
+author = 'johonkanen'
 
 release = '0.1'
 version = '0.1.0'
